5f66f003f1019b3b90e2eb138790fead6fbb9afc
FEJKEN1337 <przybekrobert@gmail.com>
2017-03-18 19:34:58 +0100

    MainMenu_Script my CSGO Game

---
+++ Assets/Script/MainMenu_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Script : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+   public  void Play_Button ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
